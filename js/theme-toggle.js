fe0928ba178271dcbe44799a61abd8722b04b226
FudgeSupreme <FudgeSupreme@users.noreply.github.com>
2026-09-11 13:04:46 +0000

    deploy: e8115ebaf1d5e3574f8b83697fbe24c7bb06c61e

--- js/theme-toggle.js
+++ js/theme-toggle.js
@@ -1,14 +1,10 @@
 function setTheme(theme) {
     document.documentElement.setAttribute("data-theme", theme);
     localStorage.setItem("theme", theme);
-  
-    document.getElementById("theme-toggle-light").classList.toggle("hidden", theme === "dark");
-    document.getElementById("theme-toggle-dark").classList.toggle("hidden", theme === "light");
   }
   
   function toggleTheme() {
-    const current = localStorage.getItem("theme") || 
-                    (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+    const current = localStorage.getItem("theme") || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
     setTheme(current === "dark" ? "light" : "dark");
   }
   
@@ -21,3 +17,4 @@ function setTheme(theme) {
     // Add event listener
     document.getElementById("theme-toggle").addEventListener("click", toggleTheme);
   })();
+
